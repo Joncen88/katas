9ccab0aa5b90f324b69539db898dd4d4544923fb
Jonathan Centeno <148176416+Joncen88@users.noreply.github.com>
2024-02-08 14:43:57 +0700

    updating  arrow function

---
+++ shortcuts/arrow functions/fixthearrow.js
@@ -0,0 +1,7 @@
+/* The following arrow function is broken. Can you fix it?
+
+const subtract (a, b) => a - b
+
+*/
+
+const  subtract = (a, b) => a - b
---
+++ shortcuts/arrow functions/fixthearrow2.js
@@ -0,0 +1,8 @@
+/*
+The following arrow function is broken. Can you fix it?
+
+const multiply = a, b => a * b
+*/
+
+
+const multiply = (a, b) => a * b
