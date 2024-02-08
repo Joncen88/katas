8545182434607e4dd6fb9cae4ce31f5f94730f75
Jonathan Centeno <148176416+Joncen88@users.noreply.github.com>
2024-02-08 14:56:13 +0700

    finished ternary operators

---
+++ shortcuts/arrow functions/fixarrow3.js
@@ -0,0 +1,5 @@
+/* The following arrow function is broken. Can you fix it? (Don't delete the curly braces).
+
+const divide = (a, b) => { a / b } */
+
+const divide = (a, b) => a / b
---
+++ shortcuts/arrow functions/simplifyarrow.js
@@ -0,0 +1,15 @@
+/* Simplify the Arrow Function
+Simplify the following arrow function to its shortest form (1 single line), and use a ternary operator.
+
+const checkPositive = (n) => {
+    if (n > 0) {
+        return true
+    } else {
+        return false
+    }
+} 
+parameters => parameter => { statements }
+*/
+
+
+const checkPositive = n => n > 0 ? true : false
