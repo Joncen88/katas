65273a7145606054a3d9b6f888f09828ad26a9d8
Jonathan Centeno <148176416+Joncen88@users.noreply.github.com>
2024-02-09 13:53:31 +0700

    search.js still working on it

--- native methods/string methods/search.js
+++ native methods/string methods/search.js
@@ -2,9 +2,9 @@
 The function should return the string from the array that contains the keyword.
 Assume that there is only 1 string containing the keyword. */
 
-function search(){
+function search(arr, txt){
 
 }
 
-console.log(search(['hello world','good morning'],'world'))
-console.log(search(['123456','911'],'9'))
+console.log(search(['hello world','good morning'],'world')) //'hello world'
+console.log(search(['123456','911'],'9')) //'911'
